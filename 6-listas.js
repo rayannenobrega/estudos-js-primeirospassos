7e727f8288119427055d4f8382aeb4c63510a864
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2021-05-12 17:27:20 -0300

    Removendo elementos

--- 6-listas.js
+++ 6-listas.js
@@ -8,3 +8,9 @@ listaDeDestinos.push(`João Pessoa`)
 
 //imprimindo um array
 console.log(listaDeDestinos);
+
+//Comando para deletar, o primeiro número é referente ao índice do elemento que se começará a deletar, o segundo número é quantos elementos você quer deletar a partir daquele índice.
+listaDeDestinos.splice(2,1);
+
+//imprimendo um elemento do array específico
+console.log(listaDeDestinos[1]);
