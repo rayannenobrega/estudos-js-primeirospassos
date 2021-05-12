abaf23618f2bfd5b8d07b62da9764a407fea2c9b
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2021-05-12 14:40:25 -0300

    guardando variáveis

--- meuPrimeiroPrograma.js
+++ meuPrimeiroPrograma.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+console.log("Meu primeiro programa: Trabalhando com variáveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
