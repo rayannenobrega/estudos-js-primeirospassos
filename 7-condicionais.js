71303d5b0b4d368204cfc89d5319df2b5d1b6845
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2021-05-12 17:56:05 -0300

    ifs encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -2,22 +2,30 @@ console.log(`Trabalhando com condicionais`);
 
 const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`, `Curitiba`);
 
-const idadeComprador = 21;
+const idadeComprador = 17;
+const estaAcompanhada = true;
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if(idadeComprador >= 18){
+if (idadeComprador >= 18) {
     console.log("Comprador maior de idade")
-    listaDeDestinos.splice(2,1);
+    listaDeDestinos.splice(2, 1);
+} else if (estaAcompanhada) {
+
+    console.log("Comprador está acompanhado")
+    listaDeDestinos.splice(2, 1);
+
 } else {
     console.log("Não é maior de idade e não posso vender");
 }
+    
+
 
 console.log(listaDeDestinos);
 
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador == 18);
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador == 18);
