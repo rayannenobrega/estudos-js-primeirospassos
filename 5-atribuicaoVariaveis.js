0dbf3d9b3d6ac3785a7b83d73c94b83c67c3432b
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2021-05-12 16:24:57 -0300

    Atribuição e imutabilidade

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Atribuição de variáveis");
+
+const idade = 29;
+let nome = "Ricardo";
+const sobrenome = "Bugan";
+
+//Mesmo resultado de espaçamento
+console.log(nome,sobrenome);
+console.log(nome+" "+sobrenome);
+console.log(`Meu nome é ${nome}`);
+
+//const é uma constante e let é variável comum.
+nome = nome + sobrenome;
+console.log(nome);
