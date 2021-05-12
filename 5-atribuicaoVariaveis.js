1bbf3dee72abf9d5df0450f3377eff4b7d10724f
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2021-05-12 17:10:58 -0300

    criando listas

--- 5-atribuicaoVariaveis.js
+++ 5-atribuicaoVariaveis.js
@@ -1,6 +1,6 @@
 console.log("Atribuição de variáveis");
 
-const idade = 29;
+//const idade = 29;
 let nome = "Ricardo";
 const sobrenome = "Bugan";
 
@@ -12,3 +12,8 @@ console.log(`Meu nome é ${nome}`);
 //const é uma constante e let é variável comum.
 nome = nome + sobrenome;
 console.log(nome);
+
+let idade; //declarando variável
+idade = 26; // atribuindo valor
+idade = idade + 1; 
+console.log(idade);
---
+++ 6-listas.js
@@ -0,0 +1,10 @@
+console.log(`Trabalhando com listas`);
+
+//Declarando um array
+const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`, `Curitiba`);
+
+//Adicionando um item na lista depois de declarada
+listaDeDestinos.push(`João Pessoa`)
+
+//imprimindo um array
+console.log(listaDeDestinos);
