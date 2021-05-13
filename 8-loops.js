44ed4e05520f2fa8b44f4f5c0d1696fd4dda0901
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2021-05-13 09:52:40 -0300

    break e debbuger

--- 8-loops.js
+++ 8-loops.js
@@ -13,13 +13,14 @@ console.log(listaDeDestinos);
 const podeComprar = (idadeComprador >= 18 || estaAcompanhada == true);
 
 let contador = 0;
+let destinoExiste = false;
 
 while(contador < 3){
     if (listaDeDestinos[contador] == destino){
-        console.log("Destino existe");
+        destinoExiste = true;
         break;
-    } else {
-        console.log("Destino não existe");
-    }
+    } 
     contador ++;
 }
+
+console.log("Destino existe: ",destinoExiste);
