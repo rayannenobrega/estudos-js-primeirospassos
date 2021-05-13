1086a83048b94ef09de58e986ec69ac3ca9c92c1
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2021-05-13 09:05:20 -0300

    operadores lógicos encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -4,25 +4,38 @@ const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`, `C
 
 const idadeComprador = 17;
 const estaAcompanhada = true;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18) {
-    console.log("Comprador maior de idade")
-    listaDeDestinos.splice(2, 1);
-} else if (estaAcompanhada) {
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade")
+//     listaDeDestinos.splice(2, 1);
+// } else if (estaAcompanhada) {
 
-    console.log("Comprador está acompanhado")
-    listaDeDestinos.splice(2, 1);
+//     console.log("Comprador está acompanhado")
+//     listaDeDestinos.splice(2, 1);
 
+// } else {
+//     console.log("Não é maior de idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhada == true) {
+    console.log("Boa viagem !")
+    listaDeDestinos.splice(2, 1);
 } else {
     console.log("Não é maior de idade e não posso vender");
 }
-    
 
+console.log("Embarque: \n\n")
+if (idadeComprador > 18 && temPassagemComprada){
+    console.log("Boa Viagem");
+} else {
+    console.log("Você não pode embarcar");
+}
 
-console.log(listaDeDestinos);
+// || ou - && e
 
 // console.log(idadeComprador > 18);
 // console.log(idadeComprador < 18);
